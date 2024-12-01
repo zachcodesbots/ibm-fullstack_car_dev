731150c7495353590241f32edfbee018d18e8242
zachcodesbots <zachhmltn@outlook.com>
2024-12-01 18:12:12 -0500

    Connected backend

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='get_dealer_details_by_id'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews_by_id'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,8 +14,8 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -105,19 +105,46 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer":dealership})
+    else:
+        return JsonResponse({"status":400, "message": "Bad Request"})
+
 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealer_reviews(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        for review in reviews:
+            review_sentiment = analyze_review_sentiments(review['review'])
+            review['sentiment'] = review_sentiment['sentiment']
+        return JsonResponse({"status":200, "reviews": reviews})
+    else:
+        return JsonResponse({"status":400, "message": "Bad Request"})
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
